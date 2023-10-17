c57517b59e602085823cd9be9d547d577ef652ee
REZEG Liess <liess.rezeg@gmail.com>
2023-10-17 13:24:55 +0200

    Changements de signature pour GetAllProducts sur toute l'application et implémentation de UpdateProductQuantities dans ProductServices.cs

--- P2FixAnAppDotNetCode/Models/Repositories/IProductRepository.cs
+++ P2FixAnAppDotNetCode/Models/Repositories/IProductRepository.cs
@@ -1,8 +1,10 @@
-﻿namespace P2FixAnAppDotNetCode.Models.Repositories
+﻿using System.Collections.Generic;
+
+namespace P2FixAnAppDotNetCode.Models.Repositories
 {
     public interface IProductRepository
     {
-        Product[] GetAllProducts();
+        List <Product> GetAllProducts();
         //Ajout méthode getProductById
         Product GetProductById(int id); 
         void UpdateProductStocks(int productId, int quantityToRemove);
